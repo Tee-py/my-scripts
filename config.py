d2325bc382ead353ae4b801bd8cf61e84624c12f
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2021-12-06 13:54:56 +0100

    done

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-daEmail = "" #put discord email here
-daPassword = "" #put discord password here
+daEmail = "emmanueloluwatobi2000@gmail.com" #put discord email here
+daPassword = "Kelechi2000" #put discord password here
 
 def getCreds():
     return(daEmail,daPassword)
--- level.py
+++ level.py
@@ -4,34 +4,30 @@ import random
 
 
 messages_list = [
-    "This project looks very interesting.",
-    "I am interested, and I will support this project until it is successful according to the plan"
-    "Don't be lazy for something good is awaiting you",
     "Keep typing for guys we can do it and keep healthy too",
-    "This is a very Nice Project.",
     "We should keep trying hard",
-    "The Pain is worth it Guys. Let's Keep Moving.",
-    "This is going to be a massive one Guys. Let's Keep Working",
-    "to the world",
-    "Hello Everyone, Try This out.",
-    "I love swalana.",
-    "A Great Country",
-    "Keep Moving Guys",
-    "We Go Hard or We Go Home Guys."
+    "Omo To make money no easy ooo😂😂",
+    "Hand don dey pain me self😂😂",
+    "We go hard or we go home",
+    "The strong shall make mouth",
+    "Make i go price Lambo👍"
 ]
 
 for i in range(10000):
-    print(f"Number 1----> {i}")
-    index = random.randrange(0, len(messages_list))
-    sendMessage(872911192820162650, messages_list[index])
-    sendMessage(888118378571640885, messages_list[index])
-    sendMessage(888118690099392522, messages_list[index])
-    sendMessage(888119773152550942, messages_list[index])
-    sendMessage(888190363683131412, messages_list[index])
-    sendMessage(888119111299772496, messages_list[index])
-    sendMessage(888472298905018368, messages_list[index])
-    sendMessage(888119532940582934, messages_list[index])
-    sendMessage(888119618269503579, messages_list[index])
-    sendMessage(888119349653692426, messages_list[index])
-    print("\n\n")
-    time.sleep(120)
+    try:
+        print(f"Number 1----> {i}")
+        index = random.randrange(0, len(messages_list))
+        sendMessage(892466763130863616, messages_list[index])
+        time.sleep(10)
+        index = random.randrange(0, len(messages_list))
+        sendMessage(892467700599754762, messages_list[index])
+        time.sleep(10)
+        index = random.randrange(0, len(messages_list))
+        sendMessage(892467271681843240, messages_list[index])
+        time.sleep(10)
+        index = random.randrange(0, len(messages_list))
+        sendMessage(892468097066348654, messages_list[index])
+        print("\n\n")
+        time.sleep(280)
+    except:
+        pass
