5e21540e3fb69274d5ff6dc9c193fe5801220d7b
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-02-17 01:37:27 +0100

    Update tiny.py

--- tiny.py
+++ tiny.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 import os
 import time
 from algosdk.v2client.algod import AlgodClient
+from algosdk import mnemonic
 
 load_dotenv()
 
 address = os.getenv("ADDRESS")
-private_key = os.getenv("PRIVATE_KEY")
+private_key = mnemonic.to_private_key(os.getenv("PRIVATE_KEY"))
 app_id = int(os.getenv("VALIDATOR_APP_ID"))
 
 algod_address = os.getenv("ALGOD_ADDRESS")
@@ -23,36 +24,48 @@ ALGO = client.fetch_asset(0)
 
 pool = client.fetch_pool(CHOICE, ALGO)
 
-while True:
-    quote = pool.fetch_fixed_input_swap_quote(ALGO(1_000_000), slippage=0.01)
-    #print(quote)
-    if quote.price*10000 >= 50:
-        print("This is The set Price")
-        break
-    print(f'CHOICE per ALGO: {quote.price*10000}')
-    print(f'CHOICE per ALGO (worst case): {quote.price_with_slippage*10000}')
-    print('\n\n')
-    time.sleep(10)
+def swap(amount: int):
+    quote = pool.fetch_fixed_input_swap_quote(CHOICE(amount*100), slippage=0.01)
+    print(quote)
+    print(private_key)
 
-
-"""
-if quote.price_with_slippage > 180:
     print(f'Swapping {quote.amount_in} to {quote.amount_out_with_slippage}')
     # Prepare a transaction group
     transaction_group = pool.prepare_swap_transactions_from_quote(quote)
-    # Sign the group with our key
-    transaction_group.sign_with_private_key(account['address'], account['private_key'])
-    # Submit transactions to the network and wait for confirmation
-    result = client.submit(transaction_group, wait=True)
 
-    # Check if any excess remaining after the swap
-    excess = pool.fetch_excess_amounts()
-    if TINYUSDC in excess:
-        amount = excess[TINYUSDC]
-        print(f'Excess: {amount}')
+    transaction_group.sign_with_private_key(address, private_key)
+    result = client.submit(transaction_group, wait=True)
+    print(result)
+    return True
+
+if __name__ == "__main__":
+    amount = input("Enter Amount Of Choice To Swap")
+    try:
+        amount = int(amount)
+        if amount <= 0:
+            print("Invalid Amount")
+        swap(amount)
+    except:
+        print("An Error Occurred. Try Again")
+
+
+
+
+
+
+
+# Check if any excess remaining after the swap
+"""
+excess = pool.fetch_excess_amounts()
+if ALGO in excess:
+    amount = excess[ALGO]
+    print(f'Excess: {amount}')
         # We might just let the excess accumulate rather than redeeming if its < 1 TinyUSDC
         if amount > 1_000_000:
             transaction_group = pool.prepare_redeem_transactions(amount)
             transaction_group.sign_with_private_key(account['address'], account['private_key'])
             result = client.submit(transaction_group, wait=True)
 """
+
+
+
